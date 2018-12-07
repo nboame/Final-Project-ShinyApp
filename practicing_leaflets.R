2fbe6a5d766a36e19eca95dcc4fa48503042e881
Meagan McCardle <meagan_mccardle@brown.edu>
2018-12-06 23:43:45 -0500

    adding markers!

--- practicing_leaflets.R
+++ practicing_leaflets.R
@@ -1,6 +1,11 @@
 library(leaflet)
+library(dplyr)
+
+data <- readRDS("beer_data_loc_all.rds")
+data <- data[1:25,]
 
 leaflet(options = leafletOptions(minZoom = 1, dragging = TRUE)) %>%
   addProviderTiles("CartoDB") %>%
   # If we don't use setView and use addMarkers it will automatically zoom to include all marked points
-  setView(lng = -52.7126, lat = 47.5615, zoom = 1) 
+  #setView(lng = -52.7126, lat = 47.5615, zoom = 1)
+  addMarkers(lng = data$lon, lat = data$lat)
