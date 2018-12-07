c4a65f32f48fa7d52406e1e09e8abd2757f357c6
Meagan McCardle <meagan_mccardle@brown.edu>
2018-12-06 23:53:09 -0500

    adding example popup label

--- practicing_leaflets.R
+++ practicing_leaflets.R
@@ -8,4 +8,4 @@ leaflet(options = leafletOptions(minZoom = 1, dragging = TRUE)) %>%
   addProviderTiles("CartoDB") %>%
   # If we don't use setView and use addMarkers it will automatically zoom to include all marked points
   #setView(lng = -52.7126, lat = 47.5615, zoom = 1)
-  addMarkers(lng = data$lon, lat = data$lat)
+  addMarkers(lng = data$lon, lat = data$lat, popup=data$UT_beer_name)
