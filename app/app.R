03f55cf519a698c203c6aca6658e3c44c030c6dd
Meagan McCardle <meagan_mccardle@brown.edu>
2018-12-08 18:17:58 -0500

    first tabs created for the shiny app - legal drinking age and beer map tabs

--- app/app.R
+++ app/app.R
@@ -1,11 +1,33 @@
 library(shiny)
 library(dplyr)
+library(markdown)
 
-beers <- readRDS("beer_data_loc_all.rds")
+#beers <- readRDS("beer_data_loc_all.rds")
 
 ui <- navbarPage("TravelBeeR",
-                 tabPanel("Legal Drinking Age"),
-                 tabPanel("Beer Map"))
+           tabPanel("Legal Drinking Age",
+                    sidebarLayout(
+                      sidebarPanel(
+                        dateInput("birthdate", "Enter your date of birth (yyyy-mm-dd): ")
+                      ),
+                      mainPanel(
+                        plotOutput("plot")
+                      )
+                    )
+           ),
+           tabPanel("Beer Map",
+                    sidebarLayout(
+                      sidebarPanel(
+                        selectInput("beerstyle", "Select your preferred beer style: ", 
+                                    c("Ale", "Lager", "Stout")
+                        )),
+                        mainPanel(
+                        plotOutput("plot")
+                      )
+                    )
+           )
+)
+
 
 server <- function(input, output) {}
 
