76a9415e6c60d5ca1d044ea947bcc1d571633cd8
Meagan McCardle <meagan_mccardle@brown.edu>
2018-12-09 16:22:32 -0500

    beer selection list - still can't get the reading in of data to work properly

--- app/app.R
+++ app/app.R
@@ -2,33 +2,60 @@ library(shiny)
 library(dplyr)
 library(markdown)
 
-#beers <- readRDS("beer_data_loc_all.rds")
+#data <- readRDS("beer_data_loc_all.rds")
 
-ui <- navbarPage("TravelBeeR",
+ui <- navbarPage(strong("TravelBeeR"),
            tabPanel("Legal Drinking Age",
                     sidebarLayout(
                       sidebarPanel(
                         dateInput("birthdate", "Enter your date of birth (yyyy-mm-dd): ")
                       ),
                       mainPanel(
-                        plotOutput("plot")
+                        plotOutput("plot"),
+                        tableOutput("table")
                       )
                     )
            ),
            tabPanel("Beer Map",
                     sidebarLayout(
                       sidebarPanel(
-                        selectInput("beerstyle", "Select your preferred beer style: ", 
-                                    c("Ale", "Lager", "Stout")
-                        )),
+                        uiOutput("styleOutput")
+                        ),
                         mainPanel(
-                        plotOutput("plot")
+                          leafletOutput("beer_map")
                       )
                     )
            )
 )
 
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  filtered <- reactive({
+    if (is.null(input$styleInput)) {
+      return(NULL)
+    } 
+    
+    data %>%
+      filter(UT_sub_style == input$styleInput)
+  })
+  
+  output$beer_map <- renderLeaflet(
+    filtered() %>%
+      leaflet(options = leafletOptions(minZoom = 1, dragging = TRUE)) %>%
+      addProviderTiles("CartoDB") %>%
+      addCircleMarkers(lng = ~lon, 
+                       lat = ~lat,
+                       popup = ~paste0("<b>", UT_beer_name, "</b>", 
+                                       "<br/>", UT_sub_style,
+                                       "<br/>", UT_brewery),
+                       radius = 3,
+                       color = "green"))
+    
+    output$styleOutput <- renderUI({
+      selectInput("styleInput", "Beer Style",
+                  sort(unique(data$UT_sub_style),
+                  selected = "IPA - Red"))
+    })
+}
 
 shinyApp(ui = ui, server = server)
